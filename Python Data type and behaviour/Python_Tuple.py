d388b634ddb4e7309b10b7a0acd3fb01fd42b798
Lalit Mishra <74181761+lalit-096@users.noreply.github.com>
2022-09-17 21:20:39 +0530

    Add files via upload

---
+++ Python Data type and behaviour/Python_Tuple.py
@@ -0,0 +1,21 @@
+#Program to get all functions of the tuple data structure
+
+'''
+    Tuple is similar to list
+
+    immutable data structure
+    duplicate values can be added
+    list is mutable while tuple is immutable
+'''
+
+tup1 = (0, 1, 2, 3, 4)
+
+tup2 = (5, 6, 7, 8, 9)
+
+#print elements of the tuple:
+print(tup1[0])
+print(tup1[2])
+
+#tuple slicing:
+print(tup1[3:])
+print(tup1[-4:-2])
